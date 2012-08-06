aa76472d434d6737159e4bcee4a7797f586829af
jsmeltzer <jeremy@webguyaz.com>
2012-08-06 16:38:00 -0700

    Corrected CDN versions of jQuery being out of date

--- part.theme-siteoptions.inc.php
+++ part.theme-siteoptions.inc.php
@@ -1,7 +1,18 @@
 <?php 
 //Get options using this example: get_option(SM_SITEOP_PREFIX.'my_field')
-
-// create admin page
+global $jquery_version;
+function wlfw_get_jquery_version() {
+	global $wp_scripts, $jquery_version;
+	$registered_scripts = $wp_scripts->registered;
+	$jquery_version = $registered_scripts['jquery']->ver;
+	
+	// shwo warning if you will be changing versions of jQuery
+	if( !stristr( get_option(SM_SITEOP_PREFIX.'jquery_source'), $jquery_version  )) {
+		global $errors;
+		$errors->add('Warning', __(sprintf('You are currently using an older version of jQuery. Setting an option in the script sources tab will cause your site to start loading the most recent version ('.$jquery_version.')'), THEME_PREFIX));
+	}
+	
+	// create admin page
 $whiteLabelOptions = new sm_options_page(array('theme_page' => TRUE, 'parent_id' => 'themes.php', 'page_title' => 'Configure Theme Customizations', 'menu_title' => 'Theme Options','id' => 'whitelabel-appearance-options'));
 	$whiteLabelOptions->add_part($pageMeta = new sm_section('page_meta', array('title'=>'Page Meta')) );
 		$pageMeta->add_part($genTag = new sm_checkbox('page_meta_generator', array('label'=>'Include Generator Meta Tag', 'value'=>'true', 'classes'=>array('onOffSwitch') )));
@@ -17,11 +28,11 @@ $whiteLabelOptions = new sm_options_page(array('theme_page' => TRUE, 'parent_id'
 		$texturize->add_part($texturize_comments = new sm_checkbox('texturize_comments', array('label'=>'Comments', 'value'=>'true', 'classes'=>array('onOffSwitch'))));
 				
 	$whiteLabelOptions->add_part($scriptSrources = new sm_section('script_sources', array('title'=>'Script Sources')) );
-		$scriptSrources->add_part($jQuery = new sm_radio_buttons('jquery_source', array('Default (local but can be overridden by plugins)'=>'default', 'Local (cannot be overridden by plugins)'=>'local', 'Google Ajax API CDN'=>'http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js', 'Microsoft CDN'=>'http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.4.min.js', 'jQuery CDN'=>'http://code.jquery.com/jquery-1.6.4.min.js' )));
-			$jQuery->label ='jQuery';
+		$scriptSrources->add_part($jQuery = new sm_radio_buttons('jquery_source', array('Default (local but can be overridden by plugins)'=>'default', 'Local (cannot be overridden by plugins)'=>'local', 'Google Ajax API CDN'=>'http://ajax.googleapis.com/ajax/libs/jquery/'.$jquery_version.'/jquery.min.js', 'Microsoft CDN'=>'http://ajax.aspnetcdn.com/ajax/jQuery/jquery-'.$jquery_version.'.min.js', 'jQuery CDN'=>'http://code.jquery.com/jquery-'.$jquery_version.'.min.js' )));
+			$jQuery->label ='jQuery ('.$jquery_version.')';
 			$jQuery->default_value = 'default';
 			
-	$whiteLabelOptions->add_part($fourOFourPage = new sm_section('404', array('title'=>'404 Page')) );
+	$whiteLabelOptions->add_part($fourOFourPage = new sm_section('404', array('title'=>'404 Page')) ); 
 		$fourOFourPage->add_part($autoSearch = new sm_checkbox('autosearch', array('label'=>'Auto Search', 'value'=>'true', 'classes'=>array('onOffSwitch'))));
 		$fourOFourPage->add_part($defaultHTML = new sm_textfield('autosearch_default_content_id', array('label'=>'Page ID of default content', 'description'=>'Dispalyed when auto search is turned off ')));
 		$fourOFourPage->add_part($maxResults = new sm_textfield('autosearch_max_results', array('label'=>'Max number of results returned')));
@@ -43,10 +54,10 @@ $whiteLabelOptions = new sm_options_page(array('theme_page' => TRUE, 'parent_id'
 		$mobile->add_part($mobile_redirect_disabler = new sm_textfield('mobile_redirect_disabler', array('label'=>'Redrection Disabled URL Parameter', 'description'=>$mobile_redirect_disabler_desc)));
 	
 	$whiteLabelOptions->add_part($layout = new sm_section('layout', array('title'=>'Layout')) );
-		$layout->add_part($grid_system = new sm_radio_buttons('grid_system', array( '960.gs (static) <a href="http://960.gs/" target="_blank">Documentation</a>'=>'960gs', 'Inuit (responsive) <a href="http://csswizardry.com/inuitcss/" target="_blank">Documentation</a>'=>'inuit' )));
+		$layout->add_part($grid_system = new sm_radio_buttons('grid_system', array( '960.gs <a href="http://960.gs/" target="_blank">Documentation</a>'=>'960gs', '960.gs Responsive <a href="http://csswizardry.com/inuitcss/#grid-builder" target="_blank">Documentation</a>'=>'inuit' )));
 		$grid_system->label ='Grid System';
 		$grid_system->default_value = '960gs';
-		$grid_system->description = 'Grid Systems streamline web development work flow by providing a structural framework of commonly used dimensions. Whitelable Framework provides you with 2 grid options. 960.gs, a static width 12 or 16 column grid and Inuit, a fluid 16 column grid. This allows you to make your website responsive by simply selecting the Inuit option above.';
+		$grid_system->description = 'Grid Systems streamline web development work flow by providing a structural framework of commonly used dimensions. Whitelable Framework provides you with 2 grid options. 960.gs, a static width 16 column grid and a modified version of Inuit, a fluid 16 column grid. This allows you to make your website responsive by simply selecting the 960.gs Responsive option above.';
 		
 		$layout->add_part($sidebar_position = new sm_radio_buttons('sidebar_position', array( 'Left'=>'left', 'Right'=>'right' )));
 		$sidebar_position->label ='Sidebar Position';
@@ -54,6 +65,10 @@ $whiteLabelOptions = new sm_options_page(array('theme_page' => TRUE, 'parent_id'
 		
 //build the options menu!
 $whiteLabelOptions->build();
+}
+add_action('init', 'wlfw_get_jquery_version');
+
+
 
 
 // remove page genereated meta tag if option turned on in site options

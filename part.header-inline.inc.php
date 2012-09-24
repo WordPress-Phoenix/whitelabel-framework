bd01d6dfb1c9f9e0a01225dd971f9362a544a144
jsmeltzer <jeremy@webguyaz.com>
2012-09-24 15:16:30 -0700

    #33	Breadcrumb divs have duplicate IDs

--- part.header-inline.inc.php
+++ part.header-inline.inc.php
@@ -26,7 +26,7 @@
   
   <div id="breadCrumbs" class="<?php wlfw_grid_row_class(16); ?>">
     <?php if ( function_exists('yoast_breadcrumb') ) {
-			$crumbs = yoast_breadcrumb('<div id="breadcrumbs" class="'.wlfw_grid_col_class(16,true).'"><span class="crumbsStart">&nbsp;</span>','<span class="crumbsEnd">&nbsp;</span></div>', false);
+			$crumbs = yoast_breadcrumb('<div class="'.wlfw_grid_col_class(16,true).'"><span class="crumbsStart">&nbsp;</span>','<span class="crumbsEnd">&nbsp;</span></div>', false);
 			echo apply_filters( 'wlfw_breadcrumbs', $crumbs );
 		} ?>
     <div class="clear after_bread_crumbs_content"></div>

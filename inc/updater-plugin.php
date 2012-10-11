5dc3721c8371624bf4e3f7daa0b2009271d55104
unknown <SCarstens@212181A.Arizona99.hti.bfn>
2012-10-11 15:01:35 -0700

    #43 fixed theme updater to work properly

--- inc/updater-plugin.php
+++ inc/updater-plugin.php
@@ -15,81 +15,7 @@ if(!empty($_GET['action']) && ($_GET['action'] == 'do-core-reinstall' || $_GET['
 	add_filter('site_transient_update_themes', 'wlfw_transient_update_themes_filter');
 }
 
-add_filter('theme_action_links', 'wlfw_append_theme_actions', 10, 2);
-function wlfw_append_theme_actions($actions, $theme = NULL) {
-	if($theme->template != 'whitelabel-framework') return $actions;
-	
-		if ( eregi("MSIE", getenv( "HTTP_USER_AGENT" ) ) || eregi("Internet Explorer", getenv("HTTP_USER_AGENT" ) ) ) {
-?>		
-	<script src="<?php echo get_template_directory_uri(); ?>/js/buster.js" type="text/javascript"></script>
-    <script>jQuery(function($){buster.wait("iframe");});</script>
-<?php	
-		}
-	?>
-    <div id="wlfw_extended_options" style="display:none; clear:both;">
-    	<?php echo apply_filters('wlfw_append_theme_actions_content', wlfw_append_theme_actions_content()); ?>
-	</div>
-	<script type="text/javascript">
-	jQuery(document).ready(function($) {
-		//jQuery('#wlfw_extended_options').siblings('.action-links').find('p').replaceWith(jQuery('#wlfw_extended_options'));
-		jQuery('#wlfw_extended_options').siblings('.action-links').find('p').remove();
-		jQuery('#wlfw_extended_options').siblings('.action-links').append(jQuery('#wlfw_extended_options'));
-		jQuery('#wlfw_extended_options').show();
-	});
-    </script>
-	<?php
-	return $actions;	
-}
-
-function wlfw_append_theme_actions_content($stylesheet = 'whitelabel-framework'){
-	global $WLFW_UPDATE_DATA;
-	if(!isset($WLFW_UPDATE_DATA)) $WLFW_UPDATE_DATA = wlfw_transient_update_themes_filter($WLFW_UPDATE_DATA);
-	
-	if(isset($WLFW_UPDATE_DATA->response[$stylesheet])) 
-		$theme = $WLFW_UPDATE_DATA->response[$stylesheet];
-	else
-		echo "Theme update utility error code: UP52";
-		
-	//if the theme is outdated, diplay the custom theme updater content
-	if(!isset($WLFW_UPDATE_DATA->up_to_date[$stylesheet])) {
-		$update_url = wp_nonce_url('update.php?action=upgrade-theme&amp;theme=' . urlencode($stylesheet), 'upgrade-theme_' . $stylesheet);
-	ob_start();
-?>
-<strong>There is a new version of Whitelabel Framework available now. <a href="<?php echo get_template_directory_uri().'/inc/localize-remote-content.php'; ?>?remote=<?php echo urlencode( 'http://github.com/WordPress-Phoenix/whitelabel-framework/issues?milestone=&page=1&state=closed'); ?>&TB_iframe=true&amp;width=80%&amp;height=506" class="thickbox" title="Whitelabel Framework">View version <?php echo $theme['new_version'] ?> details</a> or <a href="<?php echo $update_url; ?>" onclick="if ( confirm('<?php _e('Updating this theme will lose any customizations you have made (if you have not been using a child theme). \'Cancel\' to stop, \'OK\' to update.', 'wlfw'); ?>') ) {return true;}return false;">update now</a>.</strong> 
-<?php
-	return trim(ob_end_flush(), '1');
-	}//END -- if(isset($WLFW_UPDATE_DATA->response[$stylesheet]))
-	
-	//if the theme is up to date, display the custom rollback/beta version updater
-	else {
-		ob_start();
-		$other_version_url = sprintf('%s%s', wp_nonce_url( self_admin_url('update.php?action=upgrade-github-theme&theme=') . $stylesheet, 'upgrade-theme_' . $stylesheet), '&rollback=');
-		if(!empty($WLFW_UPDATE_DATA->response[$stylesheet]['beta'])) $beta = $WLFW_UPDATE_DATA->response[$stylesheet]['beta'];
-		if(!empty($beta)) $up2date = '<span style="color:red;">running beta version '.$beta.'</span>';
-		else $up2date = 'up to date with version '.$theme['new_version'];
-		
-?>
-<p>Current version is <?php echo $up2date; ?>. Try <a href="#" onclick="jQuery('#wlfw_versions').toggle();return false;">another version?</a></p>
-<div id="wlfw_versions" style="display:none; width: 100%;">
-	<select style="width: 60%;" 
-    	onchange="if(jQuery(this).val() != '') {
-        	jQuery(this).next().show(); 
-            jQuery(this).next().attr('href','<?php echo $other_version_url ?>'+jQuery(this).val()); 
-        } 
-        else jQuery(this).next().hide();
-        ">
-       	<option value="">Choose a Version...</option>
-        <option value="master">Beta</option>
-		<?php foreach(array_reverse($WLFW_UPDATE_DATA->up_to_date[$stylesheet]['rollback']) as $ver){echo'<option>'.$ver.'</option>';}?></select>
-        <a style="display: none;" class="button-primary" href="?" onclick="if( confirm('Are you sure you want to reinstall a new version of Whitelabel?') );else return false;">Install</a>
-</div>
-<?php
-		return trim(ob_end_flush(), '1');
-	}
-}
-
 function wlfw_transient_update_themes_filter($data){
-
 	global $WLFW_UPDATE_DATA;
 	if(!empty($WLFW_UPDATE_DATA)) return $WLFW_UPDATE_DATA;
 	
@@ -145,37 +71,47 @@ function wlfw_transient_update_themes_filter($data){
 		if(isset($tag->name)) $tags[] = $tag->name;
 	}
 	usort($tags, "version_compare");
-		
+
 	// check and generate download link
 	$newest_tag = end(array_values($tags));
+
+	// check for rollback
+	if(isset($_GET['rollback'])) {
+		$data->response[$theme_key]['package'] = $github_theme_uri . '/zipball/' . urlencode($_GET['rollback']);
+		$download_link = $data->response[$theme_key]['package'];
+	}
+	else {
+		$download_link = $github_api_repo_uri . '/zipball/' . $newest_tag;
+	}
+
+	// setup update array to append version info
+	$update = array();
+	$update['new_version'] = $newest_tag;
+	$update['current_version'] = $newest_tag;
+	$update['url']         = $github_theme_uri;
+	$update['package']     = $download_link;
 	if(version_compare($theme->version,  $newest_tag, '>=')){
 		// up-to-date!
 		$data->up_to_date[$theme_key]['rollback'] = $tags;
+		$data->up_to_date[$theme_key]['response'] = $update;
+	}
+	else {
+		$data->response[$theme_key] = $update;
 	}
 	
-	// check for rollback
-	if(isset($_GET['rollback']))
-		$download_link = $data->response[$theme_key]['package'] = $github_theme_uri . '/zipball/' . urlencode($_GET['rollback']);
-	else
-		$download_link = $github_api_repo_uri . '/zipball/' . $newest_tag;
-	
-	// add newest update available to $data, 
-	$update = array();
-	$update['new_version'] = $newest_tag;
-	$update['url']         = $github_theme_uri;
-	$update['package']     = $download_link;
-
 	//check if using beta (GIT master) version
 	if(version_compare($newest_tag, $theme->version, '<')){
 		// up-to-date!
 		$update['beta'] = $theme->version;
+		$update['current_version'] = $theme->version;
+		$data->up_to_date[$theme_key]['response'] = $update;
 	}
-	else
+	else {
+		//add newest update available to $data, 
 		$update['beta'] = false;
+	}
 
-	$data->response[$theme_key] = $update;
-	
-	$WLFW_UPDATE_DATA = $data;
+	$WLFW_UPDATE_DATA = clone $data;	
 	return $WLFW_UPDATE_DATA;
 }
 
@@ -205,4 +141,79 @@ add_action('http_request_args', 'wlfw_no_ssl_http_request_args', 10, 2);
 function wlfw_no_ssl_http_request_args($args, $url) {
 	$args['sslverify'] = false;
 	return $args;
+}
+
+add_filter('theme_action_links', 'wlfw_append_theme_actions', 10, 2);
+function wlfw_append_theme_actions($actions, $theme = NULL) {
+	if($theme->template != 'whitelabel-framework') return $actions;
+	
+		if ( eregi("MSIE", getenv( "HTTP_USER_AGENT" ) ) || eregi("Internet Explorer", getenv("HTTP_USER_AGENT" ) ) ) {
+?>		
+	<script src="<?php echo get_template_directory_uri(); ?>/js/buster.js" type="text/javascript"></script>
+    <script>jQuery(function($){buster.wait("iframe");});</script>
+<?php	
+		}
+	?>
+    <div id="wlfw_extended_options" style="display:none; clear:both;">
+    	<?php echo apply_filters('wlfw_append_theme_actions_content', wlfw_append_theme_actions_content()); ?>
+	</div>
+	<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		//jQuery('#wlfw_extended_options').siblings('.action-links').find('p').replaceWith(jQuery('#wlfw_extended_options'));
+		jQuery('#wlfw_extended_options').siblings('.action-links').find('p').remove();
+		jQuery('#wlfw_extended_options').siblings('.action-links').append(jQuery('#wlfw_extended_options'));
+		jQuery('#wlfw_extended_options').show();
+	});
+    </script>
+	<?php
+	return $actions;	
+}
+
+function wlfw_append_theme_actions_content($stylesheet = 'whitelabel-framework'){
+	global $WLFW_UPDATE_DATA;
+	if(!isset($WLFW_UPDATE_DATA)) $WLFW_UPDATE_DATA = wlfw_transient_update_themes_filter($WLFW_UPDATE_DATA);
+	
+	if(isset($WLFW_UPDATE_DATA->response[$stylesheet])) 
+		$theme = $WLFW_UPDATE_DATA->response[$stylesheet];
+	elseif(isset($WLFW_UPDATE_DATA->up_to_date[$stylesheet]['response']))
+		$theme = $WLFW_UPDATE_DATA->up_to_date[$stylesheet]['response'];
+	else
+		echo "Theme update utility error code: UP52";
+		
+	//if the theme is outdated, diplay the custom theme updater content
+	if(!isset($WLFW_UPDATE_DATA->up_to_date[$stylesheet])) {
+		$update_url = wp_nonce_url('update.php?action=upgrade-theme&amp;theme=' . urlencode($stylesheet), 'upgrade-theme_' . $stylesheet);
+	ob_start();
+?>
+<strong>There is a new version of Whitelabel Framework available now. <a href="<?php echo get_template_directory_uri().'/inc/localize-remote-content.php'; ?>?remote=<?php echo urlencode( 'http://github.com/WordPress-Phoenix/whitelabel-framework/issues?milestone=&page=1&state=closed'); ?>&TB_iframe=true&amp;width=80%&amp;height=506" class="thickbox" title="Whitelabel Framework">View version <?php echo $theme['new_version'] ?> details</a> or <a href="<?php echo $update_url; ?>" onclick="if ( confirm('<?php _e('Updating this theme will lose any customizations you have made (if you have not been using a child theme). \'Cancel\' to stop, \'OK\' to update.', 'wlfw'); ?>') ) {return true;}return false;">update now</a>.</strong> 
+<?php
+	return trim(ob_end_flush(), '1');
+	}//END -- if(isset($WLFW_UPDATE_DATA->response[$stylesheet]))
+	
+	//if the theme is up to date, display the custom rollback/beta version updater
+	else {
+		ob_start();
+		$other_version_url = sprintf('%s%s', wp_nonce_url( self_admin_url('update.php?action=upgrade-github-theme&theme=') . $stylesheet, 'upgrade-theme_' . $stylesheet), '&rollback=');
+		if(!empty($theme['beta'])) $beta = $theme['beta'];
+		if(!empty($beta)) $up2date = '<span style="color:red;">running beta version '.$theme['current_version'].'</span>';
+		else $up2date = 'up to date with version '.$theme['current_version'];
+		
+?>
+<p>Current version is <?php echo $up2date; ?>. Try <a href="#" onclick="jQuery('#wlfw_versions').toggle();return false;">another version?</a><?php if(!empty($_GET['debug'])) { echo'<pre style="clear:both;">';var_export($theme);echo'</pre>'; } ?></p>
+<div id="wlfw_versions" style="display:none; width: 100%;">
+	<select style="width: 60%;" 
+    	onchange="if(jQuery(this).val() != '') {
+        	jQuery(this).next().show(); 
+            jQuery(this).next().attr('href','<?php echo $other_version_url ?>'+jQuery(this).val()); 
+        } 
+        else jQuery(this).next().hide();
+        ">
+       	<option value="">Choose a Version...</option>
+        <option value="master">Beta</option>
+		<?php foreach(array_reverse($WLFW_UPDATE_DATA->up_to_date[$stylesheet]['rollback']) as $ver){echo'<option>'.$ver.'</option>';}?></select>
+        <a style="display: none;" class="button-primary" href="?" onclick="if( confirm('Are you sure you want to reinstall a new version of Whitelabel?') );else return false;">Install</a>
+</div>
+<?php
+		return trim(ob_end_flush(), '1');
+	}
 }

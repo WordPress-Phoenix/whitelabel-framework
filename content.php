542f488db1b6fba498ae238dab148c98dc01c6fd
jsmeltzer <jeremy@webguyaz.com>
2012-11-06 11:42:08 -0700

    Added constant for disabling post thumbnail output in content template

--- content.php
+++ content.php
@@ -1,7 +1,7 @@
 <?php if(get_post_meta($post->ID, '_wlfw_exclude_title', true) =='true' ) add_filter('wlfw_before_title_output', 'wlfw_remove_page_title_h1s'); ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <?php if ( has_post_thumbnail() && get_option(SM_SITEOP_PREFIX.'featured_image_on_content') == 'true' ) { the_post_thumbnail('', array('class' => 'bm20')); } ?>
+    <?php if ( has_post_thumbnail() && get_option(SM_SITEOP_PREFIX.'featured_image_on_content') == 'true' && !defined ( 'WLFW_CONTENT_POST_THUMBNAIL_DISABLED' ) ) { the_post_thumbnail('', array('class' => 'bm20')); } ?>
     <?php echo apply_filters( 'wlfw_before_title_output', '<h1>'. get_the_title(). '</h1>'); ?>
     <?php the_content(); ?>
     <div class="clear"></div>

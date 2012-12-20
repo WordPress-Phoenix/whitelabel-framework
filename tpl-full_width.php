085d358560af09d04beb0e8c18695d7f1fa63415
jsmeltzer <jeremy@webguyaz.com>
2012-12-20 12:04:24 -0700

    #48	Full width page template does not work with exclude page title function

--- tpl-full_width.php
+++ tpl-full_width.php
@@ -16,27 +16,18 @@ get_header();
 
 <div id="middleWrapper">
 
-    <div id="middleContainer" class="<?php wlfw_grid_row_class(16); ?>">
+    <div id="middleContainer" class="<?php wlfw_grid_row_class(12); ?>">
 
-        <div id="contentColWide" class="<?php wlfw_grid_col_class(16); ?>">
+        <div id="contentColWide" class="<?php wlfw_grid_col_class(12); ?>">
 
-            <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-                <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-                    <?php if ( has_post_thumbnail() ) { the_post_thumbnail('', array('class' => 'bm20')); } ?>
-
-                    <h1><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'wlfw' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-
-					<?php the_content(); ?>
-
-                    <div class="clear"></div>
-
-                </div><!-- #post-## -->
+                <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+				<?php 
+					if($wp_query->found_posts > 1){ get_template_part( 'loop', get_post_type() ); } 
+					else { get_template_part( 'content', get_post_format() ); }
+				?>
+            	<?php endwhile; ?>
 
                 <?php comments_template( '', true ); ?>
-
-            <?php endwhile; ?>
 
         </div><!-- ./contentColWide -->
 

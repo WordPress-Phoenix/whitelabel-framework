622359dd5ec47db819740fa76b4263c51f672fc6
unknown <SCarstens@212181A.Arizona99.hti.bfn>
2013-01-17 17:18:44 -0700

    partial fix of the mobile redirect page, TODO- use wp http class

--- part.mobile-redirect.devices.inc.php
+++ part.mobile-redirect.devices.inc.php
@@ -1,31 +1,26 @@
 <?php 
-
 /*
  * Init the redirect mobile devices functions
  */
- 
-global $maybe_redirect_mobile_devices;
-$maybe_redirect_mobile_devices = get_option(SM_SITEOP_PREFIX.'redirect_mobile_devices');
+$maybe_redirect_mobile_devices = esc_url(get_option(SM_SITEOP_PREFIX.'redirect_mobile_devices'));
 
 //make sure its not empty and not false (when option is not in the database)
 //turn off redirection based on URL of Mobile Website
 if(!is_admin() && !empty($maybe_redirect_mobile_devices) && $maybe_redirect_mobile_devices && (!isset($_COOKIE['mobile_viewfullsite']) || $_COOKIE['mobile_viewfullsite'] != true) ) {
 	
 	// get url of mobile page and break down into parts
-	$mobile_redirect_url = apply_filters('wlfw_mobile_redirect_url', esc_url($maybe_redirect_mobile_devices));
-	$mobile_redirect_url_parts = parse_url($mobile_redirect_url);
+	$mobile_redirect_url_parts = parse_url($maybe_redirect_mobile_devices);
 	
 	// determine if the mobile redirection page is local or an external site
 	$local_redirect=false;
 	if( !isset($mobile_redirect_url_parts['host']) || stristr($mobile_redirect_url_parts['host'], $_SERVER['HTTP_HOST']) ) {
 		$local_redirect=true;
 	}
+	
 	// if redirection page is local and we are already on it or a subpage of it do not redirect
 	if($local_redirect && stristr( $_SERVER['REQUEST_URI'], $mobile_redirect_url_parts['path']));
-	
 	// call redirect function
-	else
-		redirect_mobile_devices(); 
+	else redirect_mobile_devices(array('maybe_redirect_mobile_devices'=>$maybe_redirect_mobile_devices)); 
 }
 
 
@@ -35,43 +30,43 @@ if(!is_admin() && !empty($maybe_redirect_mobile_devices) && $maybe_redirect_mobi
  */
 
 function redirect_mobile_devices($args = array()) {
-	global $maybe_redirect_mobile_devices;
-	//optionally use a different service provider by setting variables, default is currently:
-	//detect_mobile_using_curl($service_url = 'detectmobilebrowsers.com', $html_response_tag = 'h2', $not_mobile_result_string = 'no mobile')
-	$mobile_detector = detect_mobile_using_curl();
-	
 	//allow mobile_redirect_disabler paramenter to cancel out the redirection
 	$mobile_redirect_disabler_value = get_option(SM_SITEOP_PREFIX.'mobile_redirect_disabler');
 	if(isset($_GET[$mobile_redirect_disabler_value]) && (!empty($_GET[$mobile_redirect_disabler_value]) || $_GET[$mobile_redirect_disabler_value] == 'false') ) {
-		$mobile_detector = false;
 		$_COOKIE['mobile_viewfullsite'] = true;
 		setcookie('mobile_viewfullsite', $_COOKIE['mobile_viewfullsite'], 0);
+		return false;
+	}
+	
+	//optionally use a different service provider by setting variables, default is currently:
+	extract($args);
+	if(empty($_COOKIE['browser_type'])) {
+		$mobile_detector = detect_mobile_using_curl();
+		$_COOKIE['browser_type'] = detect_mobile_using_curl();
+		setcookie('browser_type', $_COOKIE['mobile_viewfullsite'], 0);
+	}
+	else { 
+		$mobile_detector = $_COOKIE['browser_type'];
 	}
 
 	//redirect mobile device visitors to this URL
-	//$mobile_redirect_url = 'm.'.$_SERVER['HTTP_HOST'];
-	//$mobile_redirect_url = $_SERVER['HTTP_HOST'].'/mobile/';
-	$mobile_redirect_url = apply_filters('wlfw_mobile_redirect_url', esc_url($maybe_redirect_mobile_devices));
+	$mobile_redirect_url = apply_filters('wlfw_mobile_redirect_url', $maybe_redirect_mobile_devices);
 
 	//on error, display the error
-	if(is_array($mobile_detector) && isset($mobile_detector['error'])) echo $mobile_detector['error'];
+	if(is_array($mobile_detector) && isset($mobile_detector['error'])) add_action('body_enqueue', create_function('', 'echo $mobile_detector["error"];'));
 	
 	//mobile browser is detected, redirect to mobile website using HTML meta tag
 	elseif($mobile_detector) {
-		echo( '<html><head><meta http-equiv="Refresh" content="1;url='.$mobile_redirect_url.'" /><meta name="viewport" content="width=device-width, initial-scale=1"></head><body>Mobile Device Detected, redirecting you to the mobile website at '.$mobile_redirect_url.'.</body></html>' );
+		echo( '<html><head><meta http-equiv="Refresh" content="1;url='.$mobile_redirect_url.'" /></head><body>Mobile Device Detected, redirecting you to the mobile website at '.$mobile_redirect_url.'.</body></html>' );
 		exit;
 	}
 	
 	//mobile browser was not detected message output when debug is requested using URL parameter
 	elseif(isset($_GET['mobile_detector_debug'])) echo 'Mobile Device not detected'; 
-	
-	//most likely you will remove the else and do nothing when mobile is not detected.
-	//else echo 'Mobile Device not detected';
 }
 
 
 function detect_mobile_using_curl($service_url = 'detectmobilebrowsers.com', $html_response_tag = 'h2', $not_mobile_result_string = 'no mobile') {
-	
 	$ch = curl_init(); // initialize curl handle 
 	curl_setopt($ch, CURLOPT_URL,$service_url); // set url to post to 
 	curl_setopt($ch, CURLOPT_USERAGENT,$_SERVER['HTTP_USER_AGENT']);

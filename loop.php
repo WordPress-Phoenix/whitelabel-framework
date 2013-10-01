ff09564767eeb0a07cbd956e0691242afddcb45c
Seth Carstens <seth.carstens@gmail.com>
2013-10-01 16:39:08 -0700

    fixed post-info filter

--- loop.php
+++ loop.php
@@ -6,8 +6,8 @@
 $thumbnail_option = get_option('wlfw_featured_image_on_loop')
 ?>
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <h1><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'wlfw' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php if ( $thumbnail_option != 'false' && has_post_thumbnail() ) { the_post_thumbnail('thumbnail', array('class' => 'bm20 alignright')); } ?> <?php the_title(); ?></a></h1>
-    <?php if( is_single() ) echo apply_filters('wlfw_post_info', wlfw_post_info()); ?>
+    <h1><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'wlfw' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php if ( $thumbnail_option != 'false' && has_post_thumbnail() ) { the_post_thumbnail('thumbnail', array('class' => 'bm20')); } ?> <?php the_title(); ?></a></h1>
+    <?php if( is_home() || is_category() || is_tag() || is_search() ) echo apply_filters('wlfw_post_info', wlfw_post_info()); ?>
     <?php if(get_post_format() == 'gallery') the_content(); else the_excerpt(); ?>
     <div class="clear"></div>
 </div><!-- #post-## -->
